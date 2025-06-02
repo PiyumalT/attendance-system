41c6ac0ee1756f37e5e134033c4174208db8805b
PiyumalT <piyumal-se19035@stu.kln.ac.lk>
2025-06-02 12:35:46 +0530

    Update application name in environment and layout files to 'Attendance System'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Attendance System') }}</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
